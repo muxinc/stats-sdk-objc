9b3d00f0e8db274aedca3bb23bf60e511e376ada
wtao-mux <37314602+wtao-mux@users.noreply.github.com>
2018-07-10 13:01:01 -0700

    Update with latest binary for v2.0.2 (#25)

--- Frameworks/tvOS/release/MuxCoreTv.framework/Headers/MUXSDKRequestBandwidthEvent.h
+++ Frameworks/tvOS/release/MuxCoreTv.framework/Headers/MUXSDKRequestBandwidthEvent.h
@@ -1,7 +1,12 @@
-#import <MuxCore/MuxCore.h>
+#ifndef MUXSDKRequestBandwidthEvent_h
+#define MUXSDKRequestBandwidthEvent_h
+
+#import "MUXSDKPlaybackEvent.h"
+#import <Foundation/Foundation.h>
 
 extern NSString *const MUXSDKPlaybackEventRequestBandwidthEventType;
 
 @interface MUXSDKRequestBandwidthEvent : MUXSDKPlaybackEvent
-
 @end
+
+#endif
