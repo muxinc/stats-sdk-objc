3e728ec2f36a0303e87d8f52c605d21cb3d40c82
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-12-09 18:31:19 -0800

    new builds for 3.0.0

---
+++ XCFramework/MuxCore.xcframework/ios-arm64_x86_64-maccatalyst/MuxCore.framework/Versions/A/Headers/MUXSDKCustomerViewData.h
@@ -0,0 +1,13 @@
+#ifndef MUXSDKCustomerViewData_h
+#define MUXSDKCustomerViewData_h
+
+#import "MUXSDKQueryData.h"
+#import <Foundation/Foundation.h>
+
+@interface MUXSDKCustomerViewData : MUXSDKQueryData
+
+@property (nullable) NSString *viewSessionId;
+
+@end
+
+#endif
