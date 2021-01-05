a4537811d5232287177d81e7a05293a15d987327
Nidhi Kulkarni <nkulkarni@mux.com>
2021-01-05 17:42:49 -0500

    rebuild as xcframework

---
+++ XCFramework/MuxCore.xcframework/ios-arm64_x86_64-maccatalyst/MuxCore.framework/Versions/A/Headers/MUXSDKAdRequestEvent.h
@@ -0,0 +1,12 @@
+#ifndef MUXSDKAdRequestEvent_h
+#define MUXSDKAdRequestEvent_h
+
+#import "MUXSDKPlaybackEvent.h"
+#import <Foundation/Foundation.h>
+
+extern NSString * _Nonnull const MUXSDKPlaybackEventAdRequestEventType;
+
+@interface MUXSDKAdRequestEvent : MUXSDKPlaybackEvent
+@end
+
+#endif
