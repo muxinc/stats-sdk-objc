089efaafa44f77023cc8574cf76a18812b471387
Nidhi Kulkarni <nkulkarni@mux.com>
2021-02-16 15:54:35 -0500

    Static build of MuxCore for tvOS & iOS, from Buildkite: https://buildkite.com/mux/stats-sdk-obj-c-core/builds/69#dcfbba38-3408-49c7-82e2-ac4ac60234cc

---
+++ XCFramework/MuxCore.xcframework/ios-x86_64-maccatalyst/MuxCore.framework/Versions/A/Headers/MUXSDKAdThirdQuartileEvent.h
@@ -0,0 +1,12 @@
+#ifndef MUXSDKAdThirdQuartileEvent_h
+#define MUXSDKAdThirdQuartileEvent_h
+
+#import "MUXSDKPlaybackEvent.h"
+#import <Foundation/Foundation.h>
+
+extern NSString * _Nonnull const MUXSDKPlaybackEventAdThirdQuartileEventType;
+
+@interface MUXSDKAdThirdQuartileEvent : MUXSDKPlaybackEvent
+@end
+
+#endif
---
+++ XCFramework/MuxCore.xcframework/ios-x86_64-maccatalyst/MuxCore.framework/Versions/Current/Headers/MUXSDKAdPlayEvent.h
@@ -0,0 +1,12 @@
+#ifndef MUXSDKAdPlayEvent_h
+#define MUXSDKAdPlayEvent_h
+
+#import "MUXSDKPlaybackEvent.h"
+#import <Foundation/Foundation.h>
+
+extern NSString * _Nonnull const MUXSDKPlaybackEventAdPlayEventType;
+
+@interface MUXSDKAdPlayEvent : MUXSDKPlaybackEvent
+@end
+
+#endif
