981048e1e34d8b326209095f138552d5392e1af9
Scott Kidder <scott@mux.com>
2021-02-23 19:31:51 -0800

    v3.0.2 statically linked without module support

---
+++ XCFramework/MuxCore.xcframework/ios-arm64_x86_64-maccatalyst/MuxCore.framework/Versions/A/Headers/MUXSDKAdBreakEndEvent.h
@@ -0,0 +1,12 @@
+#ifndef MUXSDKAdBreakEndEvent_h
+#define MUXSDKAdBreakEndEvent_h
+
+#import "MUXSDKPlaybackEvent.h"
+#import <Foundation/Foundation.h>
+
+extern NSString * _Nonnull const MUXSDKPlaybackEventAdBreakEndEventType;
+
+@interface MUXSDKAdBreakEndEvent : MUXSDKPlaybackEvent
+@end
+
+#endif
---
+++ XCFramework/MuxCore.xcframework/ios-arm64_x86_64-maccatalyst/MuxCore.framework/Versions/A/Headers/MUXSDKAdMidpointEvent.h
@@ -0,0 +1,12 @@
+#ifndef MUXSDKAdMidpointEvent_h
+#define MUXSDKAdMidpointEvent_h
+
+#import "MUXSDKPlaybackEvent.h"
+#import <Foundation/Foundation.h>
+
+extern NSString * _Nonnull const MUXSDKPlaybackEventAdMidpointEventType;
+
+@interface MUXSDKAdMidpointEvent : MUXSDKPlaybackEvent
+@end
+
+#endif
