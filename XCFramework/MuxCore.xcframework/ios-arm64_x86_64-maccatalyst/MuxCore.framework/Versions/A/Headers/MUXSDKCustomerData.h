be7947c92a5800873bf2badd0a79e80ce14e3807
Ben Mulligan <bmulligan@konradgroup.com>
2021-08-13 17:12:09 -0400

    update https://buildkite.com/mux/stats-sdk-obj-c-core/builds/178#150cf2ab-a1d8-48bb-93f6-3c0a82620b7f

--- XCFramework/MuxCore.xcframework/ios-arm64_x86_64-maccatalyst/MuxCore.framework/Versions/A/Headers/MUXSDKCustomerData.h
+++ XCFramework/MuxCore.xcframework/ios-arm64_x86_64-maccatalyst/MuxCore.framework/Versions/A/Headers/MUXSDKCustomerData.h
@@ -5,6 +5,7 @@
 #import "MUXSDKCustomerPlayerData.h"
 #import "MUXSDKCustomerVideoData.h"
 #import "MUXSDKCustomerViewData.h"
+#import "MUXSDKCustomerViewerData.h"
 #import <Foundation/Foundation.h>
 
 @interface MUXSDKCustomerData : MUXSDKQueryData
@@ -12,8 +13,13 @@
 @property (strong, nonatomic) MUXSDKCustomerPlayerData *customerPlayerData;
 @property (strong, nonatomic) MUXSDKCustomerVideoData *customerVideoData;
 @property (strong, nonatomic) MUXSDKCustomerViewData *customerViewData;
+@property (strong, nonatomic) MUXSDKCustomerViewerData *customerViewerData;
 
-- (id) initWithCustomerPlayerData:(MUXSDKCustomerPlayerData *) playerData videoData:(MUXSDKCustomerVideoData *) videoData viewData:(MUXSDKCustomerViewData *) viewData;
+
+- (id) initWithCustomerPlayerData:(MUXSDKCustomerPlayerData *) playerData
+                        videoData:(MUXSDKCustomerVideoData *) videoData
+                         viewData:(MUXSDKCustomerViewData *) viewData
+                       viewerData:(MUXSDKCustomerViewerData *) viewerData;
 
 @end
 
