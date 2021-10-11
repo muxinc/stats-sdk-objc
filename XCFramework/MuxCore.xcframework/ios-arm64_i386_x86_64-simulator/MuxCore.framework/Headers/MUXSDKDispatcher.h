e8e32356ecbcb95c3be35cc8f56e1df807a843cd
Nidhi Kulkarni <nkulkarni@mux.com>
2021-10-11 17:27:17 -0400

    build frameworks

--- XCFramework/MuxCore.xcframework/ios-arm64_i386_x86_64-simulator/MuxCore.framework/Headers/MUXSDKDispatcher.h
+++ XCFramework/MuxCore.xcframework/ios-arm64_i386_x86_64-simulator/MuxCore.framework/Headers/MUXSDKDispatcher.h
@@ -16,15 +16,15 @@
 
 @interface MUXSDKDispatcher : NSObject<MUXSDKEventHandling> {
 }
+- (void)handleBatch:(NSString *)envKey domain:(NSString *)domain osFamily:(NSString *)os jsonDict: (NSMutableDictionary *)jsonDict callback: (id<MUXSDKNetworkRequestsCompletion>) callback;
 
-- (void)handleBatch:(NSString *)envKey domain:(NSString *)domain osFamily:(NSString *)os withJson: (NSData *)json withCallback: (id<MUXSDKNetworkRequestsCompletion>) callback;
 + (void)handleException:(NSDictionary *)json pluginName:(NSString *)pluginName pluginVersion:(NSString *)pluginVerison;
 
 #pragma mark Deprecated
 
-- (void)handle:(id<MUXSDKEventTyping>)event __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:withJson:withCallback:")));
-- (void)handleBatch:(NSString *)envKey osFamily:(NSString *)os withJson: (NSData *)json withCallback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:withJson:withCallback:")));
-
+- (void)handle:(id<MUXSDKEventTyping>)event __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:withCallback:")));
+- (void)handleBatch:(NSString *)envKey osFamily:(NSString *)os withJson: (NSData *)json withCallback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:withCallback:")));
+- (void)handleBatch:(NSString *)envKey domain:(NSString *)domain osFamily:(NSString *)os withJson: (NSData *)json withCallback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:withCallback:")));
 @end
 
 #endif
