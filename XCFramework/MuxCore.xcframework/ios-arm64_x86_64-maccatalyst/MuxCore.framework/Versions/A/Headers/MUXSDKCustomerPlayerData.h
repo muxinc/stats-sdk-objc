f94b1781017568e4c610adbb189ddbc21a561270
StephanieZuniga <40036667+StephanieZuniga@users.noreply.github.com>
2022-04-05 13:55:45 -0600

    Release v3.11.0
    * Add inferred key support for MUXVideo and MUXData users
    * Expose MUXSDKEndedEvent in the public headers
    
    Authored-by: Stephanie Zúñiga <szuniga@konradgroup.com>

--- XCFramework/MuxCore.xcframework/ios-arm64_x86_64-maccatalyst/MuxCore.framework/Versions/A/Headers/MUXSDKCustomerPlayerData.h
+++ XCFramework/MuxCore.xcframework/ios-arm64_x86_64-maccatalyst/MuxCore.framework/Versions/A/Headers/MUXSDKCustomerPlayerData.h
@@ -9,9 +9,6 @@ extern NSString * _Nonnull ENV_KEY;
 
 @interface MUXSDKCustomerPlayerData : MUXSDKQueryData<MUXSDKUpsertable>
 
-- (_Null_unspecified instancetype)init NS_UNAVAILABLE;
-+ (_Null_unspecified instancetype)new NS_UNAVAILABLE;
-
 - (nullable instancetype)initWithPropertyKey:(nonnull NSString *)propertyKey;
 - (nullable instancetype)initWithEnvironmentKey:(nonnull NSString *)envKey;
 
