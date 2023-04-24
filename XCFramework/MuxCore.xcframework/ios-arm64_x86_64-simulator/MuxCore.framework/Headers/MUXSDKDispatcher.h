0460e6a30694f486443ca3fe2a4e620ba0f9685c
Emily Dixon <edixon@mux.com>
2023-04-24 13:30:22 -0700

    Releases/v4.5.0 (#107)
    ## Updated
    * Add DRM Type to `MUXSDKCustomerViewData` so you can specify it from another source
    
    ## Deprecated
    * `MUXSDKDispatcher`'s `handleBatch beaconCollectionDomain: osFamily: jsonDict: callback:` has been deprecated in favor of an overload that takes headers for requests to the collection domain
    * `MUXSDKNetworkRequestBuilding` `buildRequestFromURL: eventsJsonDict: error:` has been deprecated in favor of an overload that takes headers for requests to the collection domain
    
    ## Improvements
    * Performance + Reliability improvements during large events

--- XCFramework/MuxCore.xcframework/ios-arm64_x86_64-simulator/MuxCore.framework/Headers/MUXSDKDispatcher.h
+++ XCFramework/MuxCore.xcframework/ios-arm64_x86_64-simulator/MuxCore.framework/Headers/MUXSDKDispatcher.h
@@ -15,21 +15,24 @@
 @end
 
 @protocol MUXSDKNetworkRequestBuilding
-- (NSMutableURLRequest *) buildRequestFromURL:(NSURL *) url eventsJsonDict:(NSDictionary *) dict error:(NSError **) error;
+- (NSMutableURLRequest *) buildRequestFromURL:(NSURL *) url eventsJsonDict:(NSDictionary *)dict requestHeaders:(NSDictionary *)headers error:(NSError **) error;
+- (NSMutableURLRequest *) buildRequestFromURL:(NSURL *) url eventsJsonDict:(NSDictionary *) dict error:(NSError **) error __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:beaconHeaders:withCallback:")));
 @end
 
 @interface MUXSDKDispatcher : NSObject<MUXSDKEventHandling, MUXSDKNetworkRequestBuilding> {
 }
-- (void)handleBatch:(NSString *)envKey beaconCollectionDomain:(NSString *)collectionDomain osFamily:(NSString *)os jsonDict: (NSMutableDictionary *)jsonDict callback: (id<MUXSDKNetworkRequestsCompletion>) callback;
+
+- (void)handleBatch:(NSString *)envKey beaconCollectionDomain:(NSString *)collectionDomain osFamily:(NSString *)os jsonDict:(NSMutableDictionary *)jsonDict beaconHeaders:(NSDictionary *)headers callback: (id<MUXSDKNetworkRequestsCompletion>) callback;
 
 + (void)handleException:(NSDictionary *)json pluginName:(NSString *)pluginName pluginVersion:(NSString *)pluginVerison;
 
 #pragma mark Deprecated
 
+- (void)handleBatch:(NSString *)envKey beaconCollectionDomain:(NSString *)collectionDomain osFamily:(NSString *)os jsonDict: (NSMutableDictionary *)jsonDict callback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:beaconHeaders:withCallback:")));
 - (void)handle:(id<MUXSDKEventTyping>)event __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:withCallback:")));
-- (void)handleBatch:(NSString *)envKey osFamily:(NSString *)os withJson: (NSData *)json withCallback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:withCallback:")));
-- (void)handleBatch:(NSString *)envKey domain:(NSString *)domain osFamily:(NSString *)os withJson: (NSData *)json withCallback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:withCallback:")));
-- (void)handleBatch:(NSString *)envKey domain:(NSString *)domain osFamily:(NSString *)os jsonDict: (NSMutableDictionary *)jsonDict callback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:beaconCollectionDomain:osFamily:jsonDict:withCallback:")));;
+- (void)handleBatch:(NSString *)envKey osFamily:(NSString *)os withJson: (NSData *)json withCallback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:beaconHeaders:withCallback:")));
+- (void)handleBatch:(NSString *)envKey domain:(NSString *)domain osFamily:(NSString *)os withJson: (NSData *)json withCallback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:domain:osFamily:jsonDict:beaconHeaders:withCallback:")));
+- (void)handleBatch:(NSString *)envKey domain:(NSString *)domain osFamily:(NSString *)os jsonDict: (NSMutableDictionary *)jsonDict callback: (id<MUXSDKNetworkRequestsCompletion>) callback __attribute__((deprecated("Please migrate to handleBatch:beaconCollectionDomain:osFamily:jsonDict:beaconHeaders:withCallback:")));;
 @end
 
 #endif
