9c8ec93cdbad41610fbf1cfec20ccd52abeb7332
Kevin Cassidy Jr <kcassidy@mux.com>
2025-02-21 10:36:34 -0800

    v5.3.0
    adds missing macCatalyst platform to package spec

--- XCFramework/MuxCore.xcframework/tvos-arm64/MuxCore.framework/Headers/MUXSDKCustomerVideoData.h
+++ XCFramework/MuxCore.xcframework/tvos-arm64/MuxCore.framework/Headers/MUXSDKCustomerVideoData.h
@@ -5,11 +5,17 @@
 #import <Foundation/Foundation.h>
 #import "MUXSDKUpsertable.h"
 
+NS_REQUIRES_PROPERTY_DEFINITIONS
 @interface MUXSDKCustomerVideoData : MUXSDKQueryData<MUXSDKUpsertable>
 
+@property (nullable) NSString *videoAffiliate;
+@property (nullable) NSString *videoBrand;
+@property (nullable) NSString *videoCodec;
+@property (nullable) NSString *videoAudioCodec;
 @property (nullable) NSString *videoCdn;
 @property (nullable) NSString *videoContentType;
 @property (nullable) NSNumber *videoDuration;
+@property (nullable) NSString *videoDynamicRangeType;
 @property (nullable) NSString *videoEncodingVariant;
 @property (nullable) NSString *videoId;
 @property (nullable) NSNumber *videoIsLive;
--- XCFramework/MuxCore.xcframework/tvos-arm64_x86_64-simulator/MuxCore.framework/Headers/MUXSDKSessionDataEvent.h
+++ XCFramework/MuxCore.xcframework/tvos-arm64_x86_64-simulator/MuxCore.framework/Headers/MUXSDKSessionDataEvent.h
@@ -9,6 +9,7 @@
 #import "MUXSDKCustomerVideoData.h"
 #import "MUXSDKCustomerPlayerData.h"
 #import "MUXSDKCustomerViewData.h"
+#import "MUXSDKCustomerViewerData.h"
 #import "MUXSDKCustomData.h"
 
 extern NSString * _Nonnull const MUXSDKSessionDataEventType;
@@ -21,6 +22,7 @@ extern NSString * _Nonnull const MUXSDKSessionDataEventType;
 @property (nonatomic, retain) MUXSDKCustomerVideoData * _Nullable customerVideoData;
 @property (nonatomic, retain) MUXSDKCustomerPlayerData * _Nullable customerPlayerData;
 @property (nonatomic, retain) MUXSDKCustomerViewData * _Nullable customerViewData;
+@property (nonatomic, retain) MUXSDKCustomerViewerData * _Nullable customerViewerData;
 @property (nonatomic, retain) MUXSDKCustomData * _Nullable customData;
 
 @end
