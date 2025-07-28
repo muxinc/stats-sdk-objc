bfe15220a3f0c1bdf88b5db6c1c7ff826f4d3a1a
Kevin Cassidy Jr <kcassidy@mux.com>
2025-07-28 13:00:09 -0700

    Releases/v5.5.0

--- Package.swift
+++ Package.swift
@@ -1,19 +1,16 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 
 import PackageDescription
 
 let package = Package(
     name: "MuxCore",
-    platforms: [
-        .iOS(.v12),
-        .macCatalyst(.v13),
-        .tvOS(.v12),
-        .visionOS(.v1)
-    ],
     products: [
-        .library(name: "MuxCore", targets: ["MuxCore"])
+        .library(name: "MuxCore", targets: ["MuxCore"]),
     ],
     targets: [
-        .binaryTarget(name: "MuxCore", path: "XCFramework/MuxCore.xcframework")
+        .binaryTarget(
+            name: "MuxCore",
+            url: "https://github.com/muxinc/stats-sdk-objc/releases/download/v5.5.0/MUXCore.xcframework.zip",
+            checksum: "95ef70a31db4e4b77698d7a7e2052b0d1f72a5679fda700118aac0de72487293"),
     ]
 )
--- XCFramework/MuxCore.xcframework/xros-arm64/MuxCore.framework/Headers/MuxCoreVision.h
+++
@@ -1,7 +0,0 @@
-// In previous releases, the umbrella header for xrOS was MUXCoreVision.h.
-// This has been left in for compatibility, but will be removed in a future release.
-
-#import <MuxCore/MUXCore.h>
-
-#define MuxCoreVisionVersionNumber MuxCoreVersionNumber
-#define MuxCoreVisionVersionString MuxCoreVersionString
