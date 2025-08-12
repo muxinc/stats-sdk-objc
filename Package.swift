61041b8d355cd871a2b9b8367911bfc9e44bf9f3
Kevin Cassidy Jr <kcassidy@mux.com>
2025-08-12 16:20:35 -0700

    v5.5.1 (#132)
    * Removes the deprecation on `MUXSDKCustomerVideoData.videoCDN` (added in v5.5.0)

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MuxCore",
-            url: "https://github.com/muxinc/stats-sdk-objc/releases/download/v5.5.0/MuxCore.xcframework.zip",
-            checksum: "d60a8f241d64224d446d80f14c9387310429ec1b8e71a6a9b69275a918c0a05f"),
+            url: "https://github.com/muxinc/stats-sdk-objc/releases/download/v5.5.1/MuxCore.xcframework.zip",
+            checksum: "0519d609b35d4a988c7893e77bee0354a642a554c54612d23c2f258d25e0f2ee"),
     ]
 )
