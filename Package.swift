0fd4c6f1b672f916e3ce1a37ab0bd5d481f98849
Emily Dixon <edixon@mux.com>
2025-10-13 17:03:46 -0700

    Update package files

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MuxCore",
-            url: "https://github.com/muxinc/stats-sdk-objc/releases/download/v5.6.0/MuxCore.xcframework.zip",
-            checksum: "4f93d008803a08c3e51c252eb48ab71d0f173cdb2cfe99e8fdeed3c0ff981e68"),
+            url: "https://github.com/muxinc/stats-sdk-objc/releases/download/v5.7.0/MuxCore.xcframework.zip",
+            checksum: "93e763e1bab3b35e9e760fe48fb9f3408c1539edda97d395a85143a9f11e0027"),
     ]
 )
