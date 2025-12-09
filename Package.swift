36fc59da21644e328c143a05f50510f575feec45
Kevin Cassidy Jr <kcassidy@mux.com>
2025-12-09 15:13:20 -0800

    Releases/v5.7.1 (#135)

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MuxCore",
-            url: "https://github.com/muxinc/stats-sdk-objc/releases/download/v5.7.0/MuxCore.xcframework.zip",
-            checksum: "93e763e1bab3b35e9e760fe48fb9f3408c1539edda97d395a85143a9f11e0027"),
+            url: "https://github.com/muxinc/stats-sdk-objc/releases/download/v5.7.1/MuxCore.xcframework.zip",
+            checksum: "c737ef893910eb8d54b1b28417a96178e775153970478fd83cc4354a93e6d8f1"),
     ]
 )
